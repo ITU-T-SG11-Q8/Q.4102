82a5adb92a109d39bc1768a5b575449eb302a401
SUPERWHYUN <superwhyun@gmail.com>
2020-02-24 13:03:31 +0900

    .

--- utils/run_tcp_peer.sh
+++ utils/run_tcp_peer.sh
@@ -1,1 +1,1 @@
-/home/whyun/anaconda3/envs/hp2p/bin/python /home/whyun/workspace/HP2P/network/hp2p/hpeer/hpeer_run.py -connection=tcp
+/home/whyun/anaconda3/envs/hp2p/bin/python /home/whyun/workspace/HP2P/network/hp2p/hpeer/hpeer_run.py -connection=tcp -gui-port=8093 
